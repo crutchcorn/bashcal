454b10b428aad6440807850245aa07b8387651b1
Sarah <git@sarah.engineer>
2023-08-17 01:31:51 +0000

    feat: add highlight to current day
    Co-authored-by: Corbin Crutchley <git@crutchcorn.dev>

--- pages/calendar.sh
+++ pages/calendar.sh
@@ -4,6 +4,13 @@ if [[ ! -f data/month ]]; then
 fi
 
 CAL_ARGS=$(cat data/month)
+if [[ "$CAL_ARGS" == "$(date +"%-m %-Y")" ]]; then
+    DAY=$(date +"%-d")
+    CURSED_CSS='
+    tbody #date-'$DAY' {
+        background: red;
+    }'
+fi
 CALENDAR_HEAD=$(cal $CAL_ARGS \
   | tail -n +2 \
   | head -n 1 \
@@ -13,8 +20,16 @@ CALENDAR_HEAD=$(cal $CAL_ARGS \
 CALENDAR_BODY=$(cal $CAL_ARGS \
   | tail -n +3 \
   | head -n -1 \
-  | sed 's@^\(.\{2\}\)\s\(.\{2\}\)\s\(.\{2\}\)\s\(.\{2\}\)\s\(.\{2\}\)\s\(.\{2\}\)\s\(.\{2\}\)\s*$@<td>\1</td><td>\2</td><td>\3</td><td>\4</td><td>\5</td><td>\6</td><td>\7</td>@g' \
+  | sed 's@^\(.\{2\}\)\s\(.\{2\}\)\s\(.\{2\}\)\s\(.\{2\}\)\s\(.\{2\}\)\s\(.\{2\}\)\s\(.\{2\}\)\s*$@<td id="date-\1">\1</td><td id="date-\2">\2</td><td id="date-\3">\3</td><td id="date-\4">\4</td><td id="date-\5">\5</td><td id="date-\6">\6</td><td id="date-\7">\7</td>@g' \
+  | sed 's@id="date-\s*\([0-9]*\)"@id="date-\1"@g' \
   | awk '{ print "<tr>"$0"</tr>" }')
+
+#^(?:\s(\w)|(\w{2})|()\s{2})\s$
+
+# (?:\s(\w)|(\w{2})|()\s{2})\s
+
+# ^(?:\s(?<one>\w)|(?<one>\w{2})|(?<one>)\s{2})\s(?:\s(?<two>\w)|(?<two>\w{2})|(?<two>)\s{2})\s(?:\s(?<three>\w)|(?<three>\w{2})|(?<three>)\s{2})\s(?:\s(?<four>\w)|(?<four>\w{2})|(?<four>)\s{2})\s(?:\s(?<five>\w)|(?<five>\w{2})|(?<five>)\s{2})\s(?:\s(?<six>\w)|(?<six>\w{2})|(?<six>)\s{2})\s(?:\s(?<seven>\w)|(?<seven>\w{2})|(?<seven>)\s{2})\s*$
+
 
 CALENDAR_TITLE=$(cal $CAL_ARGS | head -n1)
 htmx_page << EOF
@@ -28,5 +43,6 @@ htmx_page << EOF
         ${CALENDAR_BODY}
     </tbody>
   </table>
+  <style>${CURSED_CSS}</style>
 </div>
 EOF
