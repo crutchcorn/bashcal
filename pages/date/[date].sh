4ab7b124f70d7131aae8ea3a676cdb4d4067966a
Sarah <git@sarah.engineer>
2023-08-17 01:31:57 +0000

    feat: add a create method
    Co-authored-by: Corbin Crutchley <git@crutchcorn.dev>

---
+++ pages/date/[date].sh
@@ -0,0 +1,23 @@
+
+DATE="${REQUEST_PATH##*/}";
+
+# todo: sanitize 😉
+touch data/$DATE
+
+if [[ "$REQUEST_METHOD" == "POST" ]]; then
+    # add the item to the list
+    echo "${FORM_DATA[item]}" >> data/$DATE
+fi
+
+ITEMS=$(awk '{ print "<li>"$0"</li>" }' data/$DATE)
+htmx_page << EOF
+    <div id="sidebar">
+        <ul>
+            ${ITEMS}
+        </ul>
+        <form>
+            <input name="item" type="text" />
+            <button type="submit" hx-post="/date/$DATE" hx-target="#sidebar">Submit</button>
+        </form>
+    </div>
+EOF
